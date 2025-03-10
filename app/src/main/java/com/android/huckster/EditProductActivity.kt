5128b53bd1d772d7e40f141bd2dcf5056a1bde82
LAB11_USER ON WS13 <L11X18W13@citulab.net>
2025-03-10 20:31:34 +0800

    feat:Made the Edit Product Page

---
+++ app/src/main/java/com/android/huckster/EditProductActivity.kt
@@ -0,0 +1,15 @@
+package com.android.huckster
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EditProductActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_product)
+    }
+}
