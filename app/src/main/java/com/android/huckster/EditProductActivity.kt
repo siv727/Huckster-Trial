acb7b95689552dec75dd46a6d3424e8b0fafd013
LAB11_USER ON WS13 <L11X18W13@citulab.net>
2025-03-17 20:26:33 +0800

    refactor: Changed relative to linear layout

--- app/src/main/java/com/android/huckster/EditProductActivity.kt
+++ app/src/main/java/com/android/huckster/EditProductActivity.kt
@@ -2,6 +2,8 @@ package com.android.huckster
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,13 @@ class EditProductActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_product)
+
+        val unitSpinner: Spinner = findViewById(R.id.unit_spinner)
+
+        val unitOptions = arrayOf("Piece", "Pack", "Box", "Kilogram")
+
+        val adapter = ArrayAdapter(this, R.layout.spinner_item, unitOptions)
+
+        unitSpinner.adapter = adapter
     }
 }
