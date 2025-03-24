420c339038cfcff76b6eff6d8ac8c1ef25396e20
LAB11_USER ON WS13 <L11X18W13@citulab.net>
2025-03-24 18:16:17 +0800

    feat: added functionality for back button in EditProductActivity

--- app/src/main/java/com/android/huckster/EditProductActivity.kt
+++ app/src/main/java/com/android/huckster/EditProductActivity.kt
@@ -2,12 +2,16 @@ package com.android.huckster
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.huckster.utils.startHomeActivity
+import com.android.huckster.utils.startProductListActivity
 
 class EditProductActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +25,11 @@ class EditProductActivity : Activity() {
         val adapter = ArrayAdapter(this, R.layout.spinner_item, unitOptions)
 
         unitSpinner.adapter = adapter
+
+        val button_back = findViewById<ImageView>(R.id.back_settings)
+        button_back.setOnClickListener {
+            Log.e("Settings", "Back to settings")
+            startProductListActivity()
+        }
     }
 }
